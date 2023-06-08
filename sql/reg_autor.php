a6a17fe87314e5b795a1931f9a03edc1950952fd
Ksenia Vasilyeva <130368298+Ksumatveeva@users.noreply.github.com>
2023-06-08 23:31:53 +0300

    добавили обработчика ЛК
    принимает новые данные и сохраняет их в БД.

--- sql/reg_autor.php
+++ sql/reg_autor.php
@@ -1,20 +1,32 @@
 <?php
-
+session_start();
 header('Content-Type: text/html; charset=UTF-8');
 
-$mysqli = mysqli_connect("localhost", "uncvjrjz_autor", "12345autor", "uncvjrjz_autor");
+$mysqli = mysqli_connect("localhost", "uncvjrjz_0155", "123456K", "uncvjrjz_0155");
 if ($mysqli == false) {
-    print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+    print("error");
 } else {
     // print("Соединение установлено успешно");
-    $email = $_POST["email"];
-    $pass = $_POST["pass"];
+    $email = trim(mb_strtolower($_POST["email"]));
+    $pass = trim($_POST["pass"]);
 
-    $result = $mysqli->query("SELECT * FROM `usersavtor` WHERE `email` = '$email' AND `pass` = '$pass'");
-    if ($result->num_rows != 0) {
-        print("exist");
+    $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email'");
+    // if ($result->num_rows != 0) {
+    //     print("exist");
+    // } else {
+    //     $mysqli->query("INSERT INTO `usersavtor`(`email`, `pass`) VALUES ('$email', '$pass')");
+    //     print("ok");
+    // }
+    $result = $result->fetch_assoc();
+    $hash = $result["pass"];
+
+    if (password_verify($pass, $hash)) {
+        echo "ok";
+        $_SESSION["name"] = $result["name"];
+        $_SESSION["lastname"] = $result["lastname"];
+        $_SESSION["email"] = $result["email"];
+        $_SESSION["id"] = $result["id"];
     } else {
-        $mysqli->query("INSERT INTO `usersavtor`(`email`, `pass`) VALUES ('$email', '$pass')");
-        print("ok");
+        echo "user_not_found";
     }
 }
